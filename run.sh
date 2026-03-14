8b4c002fa57539518be220a5c49bacb7fea8c6fd
Yang <yf13755921935@gmail.com>
2026-03-14 12:41:53 +0800

    init

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
+
 # 启动 conda 环境
 source /root/miniforge3/bin/activate
 conda activate ai
 
-uvicorn main:app --host 0.0.0.0 --port 3300
+cd /Users/yangfan/project/ai-filter
+uvicorn server:app --host 0.0.0.0 --port 3300
